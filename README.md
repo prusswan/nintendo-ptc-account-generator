daa06b02735ea3219e6a094a8b1287f0567c81d1
Sébastien Vercammen <contact@sebastienvercammen.be>
2016-07-28 23:34:30 +0200

    Added support for name lists + random passwords.

--- README.md
+++ README.md
@@ -7,11 +7,41 @@ This project was started as a proof of concept: even multi-billion dollar compan
 More about plus signs in e-mail addresses [on StackExchange](http://security.stackexchange.com/questions/65244/what-are-the-security-reasons-for-disallowing-the-plus-sign-in-email-addresses).
 
 ## Requirements
-* Node.js
+### Prerequisites
+* [Node.js](https://nodejs.org/en/)
+* [PhantomJS 2.1.7+](http://phantomjs.org/)
+* [CasperJS 1.1.3+](http://casperjs.org/)
 
-## Installation & Usage
-1. Install the required modules:
-    ```$ npm install```
-2. Open *index.js* and edit the settings at the top of the file.
-3. Run the script:
-    ```$ npm run start```
+The binary paths of all three prerequisites must be added to your OS' PATH environment variable, making `node -v`,
+`phantomjs -v` and `casperjs --version` accessible from commandline.
+
+## Usage
+**Warning: The generator does not look for account names or e-mail addresses that are already in use.**
+If the login is already in use, it will still store the login data in `outputFile`, even if it won't work.
+
+1. Open [index.js](index.js) and edit the settings at the top of the file:
+2. Run the script with CasperJS:
+    `$ casperjs index.js`
+
+## Configuration
+### 1. Generate 10 accounts in the format USERx, where x is 0 to 9.
+This example corresponds to the default settings. It will generate 10 accounts in the same format: user0, user1, ...
+
+In [index.js](index.js):
+
+    var start = 0;                      // Start from x (NAMEx, EMAIL+x@domain.com)
+    var end = 10;                       // Up to x, but not including (exclusive)
+
+    var useNicknamesFile = false;           // Use nicknames file, or just append numbers to username?
+    var outputFile = 'accounts.txt';        // File which will contain the generated "username password" combinations.
+    var outputFormat = '%NICK% %PASS%\r\n'; // Format used to save the account data in outputFile. Supports %NICK%, %PASS%.
+
+### 2. Generate random passwords per account.
+* Set `var useRandomPassword = true;` in [index.js](index.js).
+
+### 3. Use a list of unique usernames instead of USERx combinations.
+The list of unique usernames must be stores in [nicknames.json](nicknames.json). An example is available on the repo.
+
+To create a number of accounts with custom usernames instead of user + number combinations, change [index.js](index.js):
+
+    var useNicknamesFile = true;
--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 // Requires
 var casper = require('casper').create();
+var nicknames = require('./nicknames.json');
+var fs = require('fs');
 
 // Settings
-var start = 1;  // Start from x (NAMEx, EMAIL+x@domain.com)
-var end = 20;  // Up to x, but not including (exclusive)
-var country = 'BE';  // Country code (BE)
-var dob = '1990-12-30';  // Date of birth, YYYY-mm-dd
-var username = 'username';  // User- & display name. Make sure any "(username + number)@domain.com" is 100% unique.
-var password = 'password';
-var email_user = 'email-username';  // If your email is email@domain.com, enter "email"
-var email_domain = 'hotmail.com';  // Domain of e-mail host
+var start = 0; // Start from x (NAMEx, EMAIL+x@domain.com)
+var end = 10;  // Up to x, but not including (exclusive)
+
+var useNicknamesFile = true;        // Use nicknames file, or just append numbers to username?
+var useRandomPassword = true;       // Generate a random password?
+
+var outputFile = 'accounts.txt';        // File which will contain the generated "username password" combinations.
+var outputFormat = '%NICK% %PASS%\r\n'; // Format used to save the account data in outputFile. Supports %NICK%, %PASS%.
+
+var country = 'US';                 // Country code (BE, NL, FR, ...)
+var dob = '1990-12-30';             // Date of birth, YYYY-mm-dd
+var username = 'user';              // User- & display name. Make sure any "(username + number)@domain.com" is 100% unique.
+var password = 'pass';              // Static password for all accounts. Ignored if useRandomPassword is true.
+var email_user = 'email';           // If your email is email@domain.com, enter "email"
+var email_domain = 'domain.com';   // Domain of e-mail host
 
 // App data
 var url_ptc = 'https://club.pokemon.com/us/pokemon-trainer-club/sign-up/';
@@ -29,37 +38,71 @@ casper.on("page.error", function(msg, trace) {
     this.echo("Error: " + msg, "ERROR");
 });
 
-for(var i = start; i < end; i++) {
-    console.log('[o] Starting ' + start + ' to ' + (end - 1) + '.');
+console.log('[o] Starting ' + start + ' to ' + (end - 1) + '.');
 
+for(var i = start; i < end; i++) {
     (function(ctr) {
         casper.thenOpen(url_ptc, function () {
             this.echo('[' + ctr + '] First Page: ' + this.getTitle());
-
+            
             this.fill('form[name="verify-age"]', {
                 'dob': dob,
                 'country': country
             }, true);
         }).then(function () {
-            this.echo('[' + ctr + '] Second Page: ' + this.getTitle());
+            var _pass = password;
+            var _nick = username + ctr;
 
-            this.fill('form#user-signup-create-account-form', {
-                'username': username + ctr,
-                'password': password,
-                'confirm_password': password,
-                'email': email_user + '+' + ctr + '@' + email_domain,
-                'confirm_email': email_user + '+' + ctr + '@' + email_domain,
-                'screen_name': username + ctr,
+            var formdata = {
                 'terms': true
-            }, true);
-        }).then(function() {
-            if(/Hello! Welcome/.test(this.getHTML())) {
-                this.echo('[' + ctr + '] It worked!');
-            } else {
-                this.echo('[' + ctr + '] Failed :(');
+            };
+
+            this.echo('[' + ctr + '] Second Page: ' + this.getTitle());
+            
+            // Random password?
+            if(useRandomPassword) {
+                 _pass = randomPassword();
             }
+            
+            // Use nicknames list, or (username + number) combo?
+            if(useNicknamesFile) {
+                // Make sure we have a nickname left
+                if(nicknames.length < 1) {
+                    throw Error("We're out of nicknames to use!");
+                }
+                
+                // Get the first nickname off the list & use it
+                _nick = nicknames.shift();
+                
+                formdata['username'] = _nick;
+                formdata['screen_name'] = _nick;
+                formdata['email'] = email_user + '+' + _nick + '@' + email_domain;
+                formdata['confirm_email'] = email_user + '+' + _nick + '@' + email_domain;
+            } else {
+                // Use username & counter
+                formdata['username'] = _nick;
+                formdata['screen_name'] = _nick;
+                formdata['email'] = email_user + '+' + ctr + '@' + email_domain;
+                formdata['confirm_email'] = email_user + '+' + ctr + '@' + email_domain;
+            }
+
+            // Log it in the file of used nicknames
+            var content = outputFormat.replace('%NICK%', _nick).replace('%PASS%', _pass);
+            fs.write(outputFile, content, 'a');
+            
+            formdata['password'] = _pass;
+            formdata['confirm_password'] = _pass;
+            
+            // Fill & submit
+            this.fill('form#user-signup-create-account-form', formdata, true);
+        }).then(function() {
+            this.echo('Finished ' + ctr + '.');
         });
     })(i);
 }
 
 casper.run();
+
+function randomPassword() {
+    return Math.random().toString(36).substr(2, 8);
+}
